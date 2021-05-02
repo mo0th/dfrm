c8d335c713d89957f22152eb05550d0663794c37
Soorria Saruva <soorria.ss@gmail.com>
2021-05-02 19:09:49 +1000

    set sameSite to lax for cookies

--- src/lib/auth/session.ts
+++ src/lib/auth/session.ts
@@ -5,7 +5,9 @@ import session from 'cookie-session'
 export const sessionMiddleware: ApiMiddleware = session({
   secret: process.env.SESSION_PASSWORD,
   name: 'dfrm:session',
+  secure: process.env.NODE_ENV === 'production',
   maxAge: 15 * 24 * 60 * 60 * 1000,
+  sameSite: 'lax',
 }) as any
 
 export const setUserIdInSession = (req: ApiRequest, userId: string): void => {
